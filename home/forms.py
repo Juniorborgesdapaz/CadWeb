8fde981ddf707b0e87a5cb9a040d8449c2474ad6
Juniorborgesdapaz <139707140+Juniorborgesdapaz@users.noreply.github.com>
2024-12-09 14:46:23 -0300

    testando, erro no banco de dados e na rota

---
+++ home/forms.py
@@ -0,0 +1,17 @@
+from django.forms import ModelForm
+from django import forms
+from .models import *
+
+class CategoriaForm(forms.ModelForm):
+     class Meta:
+          model = Categoria
+          fields = ['nome', 'ordem',]
+          exclude = ['senha',]  #Exclui campos especificos 
+          widgets = {
+               'nome': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome', 'style': 'margin-bottom::15px'}),
+               'ordem': forms.NumberInput(attrs={'class': 'inteiro form-control', 'placeholder': 'Ordem'}),
+          }
+          labels = {
+               'nonme': 'Informe o nome do produto: ',
+               'ordem': 'Informe o número da ordem: ',
+          }
--- home/views.py
+++ home/views.py
@@ -1,7 +1,67 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
 
 def index(request):
     return render(request,'index.html')
 
+def categoria(request): 
+    contexto = {
+        'lista': Categoria.objects.all().order_by('id'),
+    }
+    return render(request, 'categoria/lista.html', contexto)
+
+def form_categoria(request):
+    if (request.method == 'POST'):
+        form = CategoriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista')
+            # categoria = form.save(commit=False)
+            # categoria.nome = form.data['nome']
+            # categoria.ordem = form.data['ordem']
+    else: 
+        form = CategoriaForm()
+    
+    # contexto = {
+    #  'form': form, #Caso tenha mais informações para serem passada, utilizar esse contexto   
+    # }
+    return render(request, 'categoria/formulario.html', {'form': form,})
+
+
+def editar_categoria(request, pk):
+    categoria = Categoria.objects.get(pk=pk)
+
+    if (request.method == 'POST'):
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            categoria = form.save()
+            lista=[]
+            lista.append(categoria)
+            return redirect('lista')
+
+    else: 
+        form = CategoriaForm(instance=categoria)
+    
+    # contexto = {
+    #     'form': form,
+    # }
+    return render(request, 'categoria/formulario.html', {'form':form,})
+
+def delete_categotia(request, pk):
+    categoria = Categoria.objects.get(pk=pk)
+    categoria.delete()
+    form = CategoriaForm()
+    # contexto = {
+    #     'form': form,
+    # }
+    return  render(request, 'categoria/lista.html')
+
+def detalhe_categoria(request, pk):
+    categoria = Categoria.objects.get(pk=pk)
+    form = CategoriaForm(instance=categoria)
+    print(form)
+    contexto = {
+        'form': form,
+    }
+    return render(request, 'categoria/detail.html', contexto)
